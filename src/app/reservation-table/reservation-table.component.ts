89115e59e4c34c77f5128685c216111214f7bcad
Xhuend Hysa <Xhoi11hysa@gmail.com>
2021-05-09 16:13:13 +0200

    Resevation???

---
+++ src/app/reservation-table/reservation-table.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Reservation, ReservationService} from '../services/reservationService';
+
+@Component({
+  selector: 'app-reservation-table',
+  templateUrl: './reservation-table.component.html',
+  styleUrls: ['./reservation-table.component.css']
+})
+// tslint:disable-next-line:class-name
+export class ReservationTableComponent implements OnInit {
+  reservations: Reservation[] = [];
+
+  constructor(private reservationService: ReservationService) {
+  }
+
+  ngOnInit(): void {
+    this.updateReservations();
+  }
+
+  updateReservations(): void {
+    this.reservationService.getAll().subscribe(response => {
+      this.reservations = response;
+    });
+  }
+
+  onDeleteReservation(id: number): void {
+    const shouldDelete = confirm('Are you sure you want to delete it?');
+    if (shouldDelete) {
+      this.reservationService.delete(id).subscribe(response => {
+        this.updateReservations();
+      });
+    }
+  }
+
+}
